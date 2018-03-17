22cce0d1ccdb9ea409ccf61a19fc2cd4ed22f540
Sourav Das <cs1140258@iitd.ac.in>
2018-03-17 16:14:55 +0530

    decryption part added

--- simple_aes.py
+++ simple_aes.py
@@ -1,5 +1,8 @@
 s_box = {'0000':'1001', '0001':'0100', '0010':'1010', '0011':'1011', '0100':'1101', '0101':'0001', '0110':'0101', '0111':'0101', '1000':'0110', '1001':'0010', '1010':'0000', '1011':'0011', '1100':'1100', '1101':'1110', '1110':'1111', '1111':'0111'}
 
+s_inv = {'0101':'0110', '0101':'0111', '1001':'0000', '0100':'0001', '1011':'0011', '1010':'0010', '0001':'0101', '1101':'0100', '0111':'1111', '1111':'1110', '1100':'1100', '1110':'1101', '0000':'1010', '0011':'1011', '0010':'1001', '0110':'1000'}
+
+
 def rotNib(_nibbles):
 	nibble1 = _nibbles[0:4]
 	nibble2 = _nibbles[4:8]
@@ -91,6 +94,37 @@ def encrypt(plain_text, key1, key2):
 
 def decrypt(c_text, key0, key1, key2):
 
+	r2_inv = int(c_text,2)^int(key2,2)
+	r2 =  '{0:0{1}b}'.format(r2_inv,w_len*2)
+	# print "dr2", r2
+
+	shift_inv = r2[0:4]+r2[12:16]+r2[8:12]+r2[4:8]
+	# print "shift_inv", shift_inv
+
+	sbox_inv = s_inv[shift_inv[0:4]]+s_inv[shift_inv[4:8]]+s_inv[shift_inv[8:12]]+s_inv[shift_inv[12:16]]
+
+	# print "sbox_inv"
+
+	r1_inv = int(sbox_inv,2)^int(key1,2)
+	r1 =  '{0:0{1}b}'.format(r1_inv,w_len*2)
+	# print "r1", r1
+
+	inv_mix = '0010111011101110'
+
+	shift_inv1 = inv_mix[0:4]+inv_mix[12:16]+inv_mix[8:12]+inv_mix[4:8]
+	# print "shift_inv1", shift_inv1
+
+	sbox_inv1 = s_inv[shift_inv1[0:4]]+s_inv[shift_inv1[4:8]]+s_inv[shift_inv1[8:12]]+s_inv[shift_inv1[12:16]]
+	# print "sbox_inv1",sbox_inv1
+
+
+	r0_inv = int(sbox_inv1,2)^int(key0,2)
+	r0 =  '{0:0{1}b}'.format(r0_inv,w_len*2)
+	# print "plain_text_recoverd", r0
+
+	return r0
+	
+
 
 
 w_len = 8
@@ -99,7 +133,7 @@ if __name__=='__main__':
 	master_key = '0100101011110101'
 	plain_text = '1101011100101000'
 
-	keys = generate_keys(key)
+	keys = generate_keys(master_key)
 
 	key0 = keys[0]
 	key1 = keys[1]
@@ -109,8 +143,16 @@ if __name__=='__main__':
 	re_temp = int(plain_text,2)^int(key0,2)
 	input1 = '{0:0{1}b}'.format(re_temp,w_len*2)
 
-	c_text = encrypt(input1, key1, key2)
-	print c_text
+	cipher_text = encrypt(input1, key1, key2)
+	recovered_text = decrypt(cipher_text, key0, key1, key2)
+
+	print "key", master_key
+	print "original plain text", plain_text
+	print "cipher text", cipher_text
+	print "recovered_text", recovered_text
+
+	
+
 
 
 	
